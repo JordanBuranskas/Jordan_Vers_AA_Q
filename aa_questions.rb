b9b1a13fb4d2622b855ef71149fb4aecc7c7cf16
smuushi <mishih780@gmail.com>
2023-02-08 17:33:48 -0500

    doing well

--- aa_questions.rb
+++ aa_questions.rb
@@ -19,8 +19,29 @@ end
 # object#initialize will take a hash structure of info to contrust the object. 
 # attr_accessor for all classes~
 class User
+    attr_accessor :fname, :lname, :id
 
-    def initialize
+    def initialize(user_hash)
+
+        @fname = user_hash['fname']
+        @lname = user_hash['lname']
+        @id = user_hash['id']
+
+    end
+
+    def self.find_by_id(user_id)
+
+        user_data = QuestionsDBConnection.instance.execute(<<-SQL, user_id)
+            SELECT
+                *
+            FROM
+                users
+            WHERE
+                id = ?;
+
+        SQL
+
+        return User.new(user_data[0])
 
     end
 
@@ -61,3 +82,24 @@ class Question
 
 end
 
+
+class Question_follows
+
+    attr_accessor :id, :question_id, :question_follower_id
+
+    def initialize(q_follows_hash)
+
+        @id = q_follows_hash["id"]
+        @question_id = q_follows_hash["question_id"]
+        @question_follower_id = q_follows_hash["question_follower_id"]
+
+    end
+
+    def self.find_by_id(q_follows_id)
+
+        q_follows_data = QuestionsDBConnection.instance.execute
+
+    end
+
+
+end
