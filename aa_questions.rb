be194342d3a39a9ecc3c17a76b3a8eca569f3e6c
smuushi <mishih780@gmail.com>
2023-02-08 16:26:04 -0500

    class sql

---
+++ aa_questions.rb
@@ -0,0 +1,31 @@
+require 'sqlite3'
+require 'singleton'
+
+class QuestionsDBConnection < SQLite3::Database 
+    #our QuestionsDBConnection.instance will connect us to the DB. :)
+
+    include Singleton
+
+    def initialize
+        super("questions.db")
+
+        self.type_translation = true
+        self.results_as_hash = true
+
+    end
+end
+
+# ::Find_by_id --- and should contain object#new somewhere
+# object#initialize will take a hash structure of info to contrust the object. 
+# attr_accessor for all classes~
+class User
+
+    
+
+end
+
+
+class Question
+
+end
+
